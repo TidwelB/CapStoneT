11b37b5739dfa2078836f99ddba21749fafc649a
Madison <yamm@email.sc.edu>
2022-09-14 22:19:45 -0400

    Added partially broken buttons

--- main.lua
+++ main.lua
@@ -2,17 +2,78 @@
 Gamestate = require 'libraries.gamestate'
 local menu = {}
 local game = {}
+local buttons = {}
+local test = {}
 
+
+BUTTON_HEIGHT = 64
+local font = nil
+
+local function newButton(text,fn)
+    return{
+        text = text,
+        fn = fn,
+        now = false,
+        last = false
+    }
+end
 -- Initializes the main menu at a very basic level
+
+function menu:enter()
+    table.insert(buttons,newButton("Start Game",startGame()))
+    table.insert(buttons,newButton("Exit",function()love.event.quit(0)end))
+    table.insert(buttons,newButton("Test",function()Gamestate.switch(test)end))
+   
+end
+local function startGame()
+    Gamestate.switch(game)
+end
 function menu:draw()
+    --This is creating the main menu buttons and their funtions
+    local ww = love.graphics.getWidth()
+    local wh = love.graphics.getHeight()
+    local buttonwidth = ww * (1/3)
+    local margin = 16
+    local cursor_y = 0
+    local total = (BUTTON_HEIGHT+margin) * #buttons
+    for i, buttons in ipairs(buttons) do
+        buttons.last = buttons.now
+        love.graphics.setColor(0.4,.4,.5,1)
+        local x = (ww*.5)-(buttonwidth*.5)
+        local y = (wh * .5)-(total * .5)+cursor_y
+        local color = {.4,.4,.5,1}
+        local mousex, mousey = love.mouse.getPosition()
+        local highlight = mousex > x and mousex < x + buttonwidth and mousey > y and mousey < y + BUTTON_HEIGHT
+        if highlight then
+            color  = {.8,.8,.9,1}
+        end
+        buttons.now = love.mouse.isDown(1)
+        if buttons.now and not buttons.last and highlight then
+            buttons.fn()
+        end
+        love.graphics.setColor(unpack(color))
+        love.graphics.rectangle("fill",(ww*.5)-(buttonwidth*.5),(wh * .5)-(total * .5)+cursor_y,buttonwidth,BUTTON_HEIGHT)
+        love.graphics.setColor(0,0,0,1)
+        local textwidth = font:getWidth(buttons.text)
+        local textHeight = font:getHeight(buttons.text)
+        love.graphics.print(buttons.text,font,(ww*.5)-textwidth*.5,y+textHeight*.5)
+        cursor_y = cursor_y + (BUTTON_HEIGHT + margin)
+    end
+    
+   --table.insert(buttons, newButton("Start Game", function()Gamestate.switch(game)end))
+    --table.insert(buttons,newButton("Exit",function()love.event.quit(0)end))
+    --love.graphics.print("Press enter to play", 10, 10)
+end
+function test:enter()
+    love.window.setMode(200,200)
     love.graphics.print("Press enter to play", 10, 10)
 end
 
-function menu:keyreleased(key, code)
-    if key == 'return' then
-        Gamestate.switch(game)
-    end
-end
+--function menu:keyreleased(key, code)
+  -- if key == 'return' then
+    --    Gamestate.switch(game)
+    --end
+--end
 
 function game:enter()
     -- Hitbox library
@@ -150,5 +211,10 @@ end
 -- prepares the game for switches
 function love.load()
     Gamestate.registerEvents()
+    font = love.graphics.newFont(32)
     Gamestate.switch(menu)
+    
+   -- table.insert(buttons,newButton("Leave",function()love.event.quit(0)end))
+    
+   
 end
