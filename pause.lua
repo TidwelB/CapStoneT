e4fcd2db33e76b78f633d662a334110ee5054ce0
Madison <yamm@email.sc.edu>
2022-10-05 20:37:48 -0400

    Pause menu overlay

--- pause.lua
+++ pause.lua
@@ -2,9 +2,20 @@
 Gamestate = require 'libraries.gamestate'
 pause = {}
 pause = Gamestate.new()
-
+local buttons = {}
+BUTTON_HEIGHT = 64
+local font = nil
+local function newButton(text,fn)
+    return{
+        text = text,
+        fn = fn,
+        now = false,
+        last = false
+    }
+end
 function pause:enter(from)
     self.from = from
+   -- love.window.setMode(1024,1024)
     --todo
     --player.load()
 
@@ -13,22 +24,37 @@ function pause:update(dt)
     pause.update(dt)
 end
 function pause:draw()
-    local w,h = love.graphics.getWidth(),love.graphics.getHeight()
-    --self.from:draw()
-
-    love.graphics.setColor(0,0,0,100)
-    love.graphics.rectangle('fill', 0,0,w,h)
-    love.graphics.setColor(255,255,255)
-    love.graphics.printf('PAUSE',0,h/2,w,'center')
+    love.graphics.reset()
+  local ww,wh = love.graphics.getWidth(),love.graphics.getHeight()
+    self.from:draw()
+    font = love.graphics.newFont(32)
+    --love.graphics.setColor(0,0,0,100)
+    --love.graphics.setColor(.4,.4,.5,.1)
+   -- love.graphics.rectangle('fill', 0,0,ww,wh)
+ --   love.graphics.setColor(255,255,255)
+   -- love.graphics.reset()
+    love.graphics.printf('PAUSED',0,wh/2,ww,'center')
+    love.graphics.printf('To Continue Press: p',0,wh/3,ww,'center')
+    love.graphics.printf('To Exit Press: Escape',0,wh/3+wh/3,ww,'center')
+    if love.keyboard.isDown('escape') then
+        love.event.quit(0)
+    end
 end
 
+-- function pause:load()
+   
+--     --Gamestate.switch(pause)
+--     font = love.graphics.newFont(32)
+--     love.graphics.setBackgroundColor(255,255,255,1)
+--     table.insert(buttons,newButton("Game",function()love.event.quit(0)end))
+--     table.insert(buttons,newButton("Sadness",function()love.event.quit(0)end))
+-- ends
 
-
-
-function pause.update(dt)
+function pause:update()
 
     if love.keyboard.isScancodeDown('p') then
         love.timer.sleep(.15)
         return Gamestate.pop()
     end
+    
 end
