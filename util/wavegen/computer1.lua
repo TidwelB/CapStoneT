4a2e80df4a7bbb70d13b009ca4733c433ccec610
Reid Mozley <wmozley@email.sc.edu>
2023-02-24 15:23:25 -0500

    edits

--- util/wavegen/computer1.lua
+++ util/wavegen/computer1.lua
@@ -1,7 +1,14 @@
-function computer1:compute() 
+local sine = require('util.wavegen.sinewave')
+local saw = require('util.wavegen.sawwave')
+local square = require('util.wavegen.squarewave')
+local triangle = require('util.wavegen.trianglewave')
+
+function computer1:compute()
+    local wave = sine
+    
 end
 
-function computer1:update(dt) 
+function computer1:update(dt)
 end
 
 function computer1:switch()
