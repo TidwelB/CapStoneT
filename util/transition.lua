2130d349b82d8c3746a0c0725685f122ab774250
tidwelb <tidwelb@email.sc.edu>
2023-02-24 14:13:27 -0500

    Transitions between lvlOne and game lobby

--- util/transition.lua
+++ util/transition.lua
@@ -9,9 +9,31 @@ end
 
 function transition:Transitioner()
     if player.collider:enter('Ghost') then
-        transition:update()
-        room = "levelOne"
-        return Gamestate.switch(runLevelOne)
+        if player.x > 1300 and player.x < 1550 and room =="levelOne" then
+            room = "game"
+            return Gamestate.switch(runGame)
+        end
+print(player.x)
+        --transition:update()
+print(Gamestate.current() == runGame)
+        if Gamestate.current() == runGame then
+            room = "levelOne"
+            return Gamestate.switch(runLevelOne)
+
+
+        
+        end
+
+
+
+
+        --1 - main  1349,1324
+        --2 - main   738, 5.33
+        --2 - 3      742,1512
+        --2 - maze      777,10
+        --3 - 4      774,1508
+        --4 - 3     775, 10
+
     end
 end
 
