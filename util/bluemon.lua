f7720387b97388dac437c29d592cfa55487bac48
Madison <yamm@email.sc.edu>
2023-02-24 20:45:38 -0500

    Worked on puzzle 3 screens and solution #98

--- util/bluemon.lua
+++ util/bluemon.lua
@@ -1,6 +1,6 @@
 bluemon = {}
 bluemon = Gamestate.new()
-
+secret = love.graphics.newImage("screens/secret.png")
 function bluemon:enter(from)
     self.from = from
     self.word = ""
@@ -11,26 +11,37 @@ function bluemon:update(dt)
 end
 
 function bluemon:draw()
-    love.graphics.reset()
-    --self.from:draw()
-    love.graphics.printf("Enter the secret tunnel:", 0, love.graphics.getHeight()/2 - 64, love.graphics.getWidth(), "center")
-    love.graphics.printf(self.word, 0, love.graphics.getHeight()/2, love.graphics.getWidth(), "center")
+    self.from:draw()
+   -- love.graphics.reset()
+
+   local screenWidth, screenHeight = love.graphics.getWidth(), love.graphics.getHeight()
     
---love.graphics.printf(input, 0, love.graphics.getHeight()/2, love.graphics.getWidth(), "center")
+   love.graphics.draw(secret, 0, 0, 0, screenWidth / secret:getWidth(), screenHeight / greenpuzzle:getHeight())
+   if love.keyboard.isDown('escape') then
+       love.timer.sleep(.15)
+       Gamestate.pop()
+   end
+    love.graphics.printf("Enter the secret tunnel:", 0, love.graphics.getHeight()/2 - 20, love.graphics.getWidth(), "center")
+    love.graphics.printf(self.word, 0, love.graphics.getHeight()/2 + 10, love.graphics.getWidth(), "center")
 end
 
 function bluemon:textinput(t)
-    -- append typed characters to the filename
     self.word = self.word .. t
 end
 
 function bluemon:keypressed(key)
     if key == "backspace" then
-        -- remove the last character from the filename
         self.word = self.word:sub(1, #self.word-1)
     elseif key == "return" then
-        if self.word == "cry" then
-            Gamestate.push(win)
+        if self.word == "cry " then
+            Gamestate.pop(bluemon)
+            --need to unlock/draw item that is from solving puzzle
+        else
+            Gamestate.push(lose)
         end
     end
+end
+
+function bluemon.load()
+    secret = love.graphics.newImage("screens/secret.jpg")
 end
