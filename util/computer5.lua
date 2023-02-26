2b885f816e715462570fdd9f81e1a8d33a58733b
Reid Mozley <wmozley@email.sc.edu>
2023-02-26 17:08:19 -0500

    changed font size

--- util/computer5.lua
+++ util/computer5.lua
@@ -14,7 +14,7 @@ function computer5:draw()
     
     love.graphics.clear(0.2, 0.2, 0.2) -- clear the screen with a dark gray color
     love.graphics.setColor(1, 1, 1)
-
+    love.graphics.setFont(30)
     love.graphics.print("Welcome to the computer lab! To get to the key item, you need to activate all four computers.", 100, 10)
     love.graphics.print("The first computer is in the top left corner, and it's a bit picky about its waveform. It likes something that's jagged but not too harsh. Can you give it the right wave?", 100, 100)
     love.graphics.print("The second computer is in the top right corner, and it's a bit of a rebel. It doesn't like the usual waveforms. Instead, it wants something that's edgy and unconventional. Can you figure out what kind of wave it wants?", 100, 200)
