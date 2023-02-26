875787bc87a9166306f149870a2918553406feb3
Reid Mozley <wmozley@email.sc.edu>
2023-02-26 17:08:52 -0500

    fixed a  bug on font sizes

--- util/computer5.lua
+++ util/computer5.lua
@@ -9,12 +9,11 @@ function computer5:enter(from)
   end
 
 function computer5:draw()
-    self.from:draw()
+    SSSself.from:draw()
     local screenWidth, screenHeight = love.graphics.getWidth(), love.graphics.getHeight()
     
     love.graphics.clear(0.2, 0.2, 0.2) -- clear the screen with a dark gray color
     love.graphics.setColor(1, 1, 1)
-    love.graphics.setFont(30)
     love.graphics.print("Welcome to the computer lab! To get to the key item, you need to activate all four computers.", 100, 10)
     love.graphics.print("The first computer is in the top left corner, and it's a bit picky about its waveform. It likes something that's jagged but not too harsh. Can you give it the right wave?", 100, 100)
     love.graphics.print("The second computer is in the top right corner, and it's a bit of a rebel. It doesn't like the usual waveforms. Instead, it wants something that's edgy and unconventional. Can you figure out what kind of wave it wants?", 100, 200)
