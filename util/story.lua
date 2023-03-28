f7a093a1eab5ca176b1b9175cace95dd7f8a3ed0
tidwelb <tidwelb@email.sc.edu>
2023-03-27 22:55:03 -0400

    Text UI for new games improved

--- util/story.lua
+++ util/story.lua
@@ -2,29 +2,86 @@ Gamestate = require 'libraries.gamestate'
 story = {}
 story = Gamestate.new()
 
-function story:enter(from)
-      self.from = from
-      love.window.setMode(1920, 1080, {resizable=true, vsync=true, minwidth=400, minheight=300})
-  end
 
-function story:draw()
-    self.from:draw()
-    local screenWidth, screenHeight = love.graphics.getWidth(), love.graphics.getHeight()
-    
-    love.graphics.clear(0, 0, 0) -- clear the screen with a dark gray color
-    love.graphics.setColor(1, 1, 1)
-    love.graphics.print("This will be the story introduction", 100, 10)
-    love.graphics.print("Someone can write this if they see fit", 100, 100)
-    love.graphics.print("Important things to write would be that they need to talk to the scienctist once the game starts", 100, 200)
-    love.graphics.print("As well as that to leave this screen they need to hit enter", 100, 300)
-    love.graphics.print("Additionally anyhting about the game story would be amazing to add to this screen", 100, 400)
-    love.graphics.print("This screen is in util.story for anyone who wants to code this", 100, 500)
-    --will need to look into pausing this screen?
-    --as well as saving?
-    --anythign else?
+positions = {
+    {50, 50},
+    {50, 100},
+    {50, 150},
+    {50, 200}
+}
+text = {
+    "This will be the story introduction",
+    "Someone can write this if they see fit",
+    "Important things to write would be that they need to talk to the scientist once the game starts",
+    "As well as that to leave this screen they need to hit enter"
+}
+charIndex = {0, 0, 0, 0}
+lineIndex = 1
+linePrinted = {false, false, false, false}
+timeElapsed = 0
+delay = 0.02
+isFinished = false
 
+
+  function story.load()
+    love.graphics.setFont(love.graphics.newFont(20))
+    positions = {
+        {50, 50},
+        {50, 100},
+        {50, 150},
+        {50, 200}
+    }
+    text = {
+        "This will be the story introduction",
+        "Someone can write this if they see fit",
+        "Important things to write would be that they need to talk to the scientist once the game starts",
+        "As well as that to leave this screen they need to hit enter"
+    }
+    charIndex = {0, 0, 0, 0}
+    lineIndex = 1
+    linePrinted = {false, false, false, false}
+    timeElapsed = 0
+    delay = 0.02
+    isFinished = false
+end
+
+function story:update(dt)
+    if isFinished then return end
+
+    timeElapsed = timeElapsed + dt
+    if timeElapsed > delay then
+        timeElapsed = timeElapsed - delay
+        charIndex[lineIndex] = charIndex[lineIndex] + 1
+        if charIndex[lineIndex] > #text[lineIndex] then
+            linePrinted[lineIndex] = true
+        end
+    end
     if love.keyboard.isDown('return') then
         love.timer.sleep(.15)
         Gamestate.switch(runGame)
     end
 end
+
+function story:draw()
+    for i = 1, #text do
+        if i < lineIndex then
+            love.graphics.print(text[i], positions[i][1], positions[i][2])
+        elseif i == lineIndex then
+            if linePrinted[i] then
+                love.graphics.print(text[i], positions[i][1], positions[i][2])
+            else
+                love.graphics.print(string.sub(text[i], 1, charIndex[i]), positions[i][1], positions[i][2])
+            end
+        else
+            love.graphics.print("Press 'return' to continue", positions[#text][1], positions[#text][2] + 50)
+        end
+    end
+    if linePrinted[lineIndex] then
+        charIndex[lineIndex] = 0
+        lineIndex = lineIndex + 1
+        if lineIndex > #text then
+            Gamestate.switch(runGame)
+            end
+        end
+    end
+
